a9c4c91c69565cbc851ebdc2f9351ae1db52b54b
rq-oza-n-at-321899771753 <hongmengxiaoze29@gmail.com>
2025-12-12 19:51:02 +0900

    Fix: ワーカー数を2に固定

--- config/puma.rb
+++ config/puma.rb
@@ -11,27 +11,38 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Specifies that the worker count should equal the number of processors in production.
-if ENV["RAILS_ENV"] == "production"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
-end
-
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-# port ENV.fetch("PORT") { 3000 }
-
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT') { 3000 }}"
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+# 本番環境の設定
+if ENV.fetch("RAILS_ENV") == "production"
+  # ワーカー数を2に固定(Renderの無料プランに最適)
+  workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+  # バインドアドレスを明示的に指定
+  bind "tcp://0.0.0.0:#{ENV.fetch('PORT')}"
+
+  # プリロードの設定
+  preload_app!
+
+  # ワーカー起動時の設定
+  on_worker_boot do
+    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+  end
+else
+  # 開発環境ではbindを使用
+  bind "tcp://0.0.0.0:#{ENV.fetch('PORT') { 3000 }}"
+end
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
