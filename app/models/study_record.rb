89f0b700a824fc3a25bfaec024e4540741569230
rq-oza-n-at-321899771753 <hongmengxiaoze29@gmail.com>
2025-12-18 12:10:02 +0900

    Add: 学習記録のロジックを記述

--- app/models/study_record.rb
+++ app/models/study_record.rb
@@ -5,10 +5,42 @@ class StudyRecord < ApplicationRecord
   validates :content, presence: true
   validates :category, presence: true
   validates :studied_at, presence: true
+  validates :next_review_at, presence: true
+  validates :review_count, numericality: { greater_than_or_equal_to: 0 }
+  validates :last_reviewed_at, presence: true, if: -> { review_count.positive? }
 
+# レコード作成時に初回の復習日を設定するコールバック
+
+
+  after_create :set_initial_review_date
+# after_update :update_next_review_date, if: :saved_change_to_review_count?
+
+# 復習実行時に呼ばれ、復習回数と最終復習日時を更新するメソッド
   def mark_reviewed!
     self.review_count += 1
     self.last_reviewed_at = Time.current
     save!
   end
+
+  private
+  # 初回の復習日を設定するメソッド
+  def set_initial_review_date
+    update!(
+      review_count: 0,
+      next_review_at: studied_at + 1.day
+    )
+  end
+
+  # レビューの回数に応じて復習する日付を計算するメソッド
+  def calculate_next_review_date
+    case review_count
+    when 0
+      studied_at + 1.day
+    when 1
+      studied_at + 3.days
+    else
+      studied_at + 7.days
+    end
+  end
+
 end
