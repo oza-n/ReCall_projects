4c20aecf9b7eda6f97aeb196f80d8589b2a5b02c
rq-oza-n-at-321899771753 <hongmengxiaoze29@gmail.com>
2025-12-20 00:34:08 +0900

    Fix: Rubocopを実行

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module App
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-
     config.i18n.default_locale = :ja
     config.i18n.available_locales = %i[en ja]
     # Configuration for the application, engines, and railties goes here.
